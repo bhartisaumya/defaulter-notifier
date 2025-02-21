b979cd173385d565c1bbed7a75aa19b66ebde84d
arko385 <90547765+arko385@users.noreply.github.com>
2025-02-21 20:00:48 +0530

    login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Route,Routes,BrowserRouter} from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import Login from './Components/login/Login.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <BrowserRouter>
+    <Routes>
+    <Route path="login" element={<Login />} />
+   
+    </Routes>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
