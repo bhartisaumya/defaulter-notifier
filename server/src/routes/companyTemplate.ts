0a3cc0834eda5a8e5c60ab42fe72d8caeef31478
Saumya Bharti <bhartisaumya22@gmail.com>
2025-02-21 16:30:03 +0530

    added authorization

--- server/src/routes/companyTemplate.ts
+++ server/src/routes/companyTemplate.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import {getTemplate, addNewTemplate, deleteTemplate, updateTemplate} from "../controller/companyTemplate";
 
+import {restrictTo} from "../middlewares/authentication";
+import { Role } from "../models/users";
+
 const router = Router();
 
-router.get("/", getTemplate);
-router.post("/", addNewTemplate);
-router.patch("/", updateTemplate);
-router.delete("/", deleteTemplate);
+router.get("/", restrictTo([Role.ADMIN, Role.USER]), getTemplate);
+router.post("/", restrictTo([Role.ADMIN]), addNewTemplate);
+router.patch("/", restrictTo([Role.ADMIN]), updateTemplate);
+router.delete("/", restrictTo([Role.ADMIN]), deleteTemplate);
 
 
 export default router;
--- server/src/routes/companyUsers.ts
+++ server/src/routes/companyUsers.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {getAllUsers, addNewCompanyUser, updateCompanyUser, deleteCompanyUser} from "../controller/companyUsers";
 
+import {restrictTo} from "../middlewares/authentication";
+import { Role } from "../models/users";
 const router = Router();
 
-router.get("/", getAllUsers);
-router.post("/", addNewCompanyUser);
-router.patch("/", updateCompanyUser);
-router.delete("/", deleteCompanyUser);
+router.get("/", restrictTo([Role.SUPER_ADMIN]), getAllUsers);
+router.post("/", restrictTo([Role.SUPER_ADMIN, Role.ADMIN]), addNewCompanyUser);
+router.patch("/", restrictTo([Role.SUPER_ADMIN, Role.ADMIN]), updateCompanyUser);
+router.delete("/", restrictTo([Role.SUPER_ADMIN, Role.ADMIN]), deleteCompanyUser);
 
 
 export default router;
