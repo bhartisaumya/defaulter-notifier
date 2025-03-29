b63b32a5ef0bda278220dcdc4ecc95efbc583998
Saumya Bharti <bhartisaumya22@gmail.com>
2025-03-29 16:25:58 +0530

    chore: Removed unused imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { Provider } from 'react-redux';
-import store from './store';
-
-
-const domain = import.meta.env.VITE_AUTH0_DOMAIN
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
       <App />
-  </Provider>
 )
